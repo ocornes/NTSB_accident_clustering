90de2382fc7e8d51f40d41d42591b3fc21069208
Thomas-Paul Roberts <tomproberts@outlook.com>
2022-06-13 13:52:11 +0200

    Improved error handling

--- src/Data_extraction_pipeline.py
+++ src/Data_extraction_pipeline.py
@@ -5,8 +5,12 @@ import requests
 import os
 from tqdm import tqdm
 
+from Output_errors import output_errors
+
 default_input = 'test_55_hits.xml'  # The xml file containing all the accidents
 target_folder = 'dataset/'
+overwrite = True
+error_file = ''
 
 # Get XML input file
 args = sys.argv[1:]
@@ -36,7 +40,9 @@ if len(tags) == 0:
     print(f"[!] No accident ids present in '{XML_filename}'")
     exit(0)
 
-failed_ids = []
+false_ids = []  # 404 error, report doesn't exist
+malformed_ids = []  # report has blank sections in expected positions
+unknown_errors = []  # ids that failed for unknown reasons
 
 # URL for NTSB api
 url = "https://app.ntsb.gov/pdfgenerator/ReportGeneratorFile.ashx?AKey=1&RType=HTML&IType=CA&EventID="
@@ -48,11 +54,16 @@ for tag in tqdm(tags):
     event_id = tag["EventId"]
 
     # Skip if already downloaded
-    if os.path.exists(target_folder + event_id + '.txt'):
+    if not overwrite and os.path.exists(target_folder + event_id + '.txt'):
         continue
 
     # Fetch the HTML file:
     html_raw = requests.get(url=(url + event_id)).text
+
+    # Check for 404
+    if html_raw == 'No report found for the given identifiers.':
+        false_ids.append(event_id)
+        continue
 
     # Extract the text of interest:
     parsed_html = BeautifulSoup(html_raw, 'html.parser')
@@ -60,19 +71,25 @@ for tag in tqdm(tags):
     try:
         body_tag = parsed_html.findAll("body")[0]
         main_list = body_tag.contents[0].contents
-        accident_text = main_list[5].text  # 5th element is the accident description
+        analysis_text = main_list[5].text  # 5th element is the accident description
+
+        if analysis_text == ' ':
+            # malformed report, fix manually
+            malformed_ids.append(event_id)
+            continue
 
         # Save the text of interest as a textfile with accident_id name:
         file = open(target_folder + event_id + ".txt", 'w')
-        file.write(accident_text)
+        file.write(analysis_text)
         file.close()
 
-    except IndexError:
-        failed_ids.append(event_id)
+    except Exception as e:
+        unknown_errors.append((event_id, e))
         continue
 
 fd.close()  # Close the XML file
 
-print("[i] The following event_id's could not be found:")
-for failed_id in failed_ids:
-    print(f"\t{failed_id}")
+output_errors(error_file=error_file,
+              unknown=unknown_errors,
+              malformed=malformed_ids,
+              not_found=false_ids)
---
+++ src/Output_errors.py
@@ -0,0 +1,37 @@
+def output_errors(**kwargs):
+    # Args
+    error_file = kwargs.get("error_file") or ''
+    unknown = kwargs.get("unknown") or []
+
+    fd = None
+    if error_file != '':
+        fd = open(error_file, "w")
+        fd.write("<DATA>\n<ROWS>\n")
+
+    # Go through all lists of errors provided
+    for error_type, errors in kwargs.items():
+        # error_file is not a list, unknown is handled separately
+        if error_type == "error_file" or error_type == "unknown":
+            continue
+        if len(errors) > 0:
+            print(f"[i] The following event_ids were {error_type}:")
+            for failed_id in errors:
+                print(f"\t{failed_id}")
+                id_to_file(fd, failed_id, error_type)
+
+    if len(unknown) > 0:
+        print("[i] The following event_ids failed for unknown reasons:")
+        for failed_id, e in unknown:
+            print(f"\t{failed_id}, reason: {e}")
+            id_to_file(fd, failed_id, 'unknown', e)
+
+    if fd is not None:
+        fd.write("</ROWS>\n</DATA>\n")
+        fd.close()
+
+
+def id_to_file(fd, failed_id, reason, additional=''):
+    if fd is not None:
+        if additional != '':
+            reason = f"{reason}, ({additional})"
+        fd.write(f"<ROW EventId=\"{failed_id}\" Reason=\"{reason}\"/>\n")
